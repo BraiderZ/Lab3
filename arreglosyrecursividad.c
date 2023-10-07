2dcb37c990040250ec462e1abd2bb4cf0e4e3c41
BraiderZ <luisjbc2112@gmail.com>
2023-10-07 14:46:02 -0600

    Se obtuvo la longitud del arreglo

--- arreglosyrecursividad.c
+++ arreglosyrecursividad.c
@@ -2,9 +2,18 @@
 
 int main (){
 	int arreglo[] = {2, 4, 6, 23, 56, 79};
-	int valor;
+	
+	printf("%d",arreglo[2]);
+	
+	int longitud = sizeof(arreglo)/sizeof(arreglo[0]);
+	
+	printf("longitud del arreglo: %d",longitud);
 
+	int valor;
+	
 	printf("Ingrese un valor entero para verificar si se encuentra en el arreglo: ");
-    	scanf("%d", &valor);
-	printf("EL valor entero agregado es: %d",valor);
+    	
+	scanf("%d", &valor);
+	
+	printf("Valor: %d",valor);
 }
