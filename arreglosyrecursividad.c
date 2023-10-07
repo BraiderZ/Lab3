1520635636f5dfcdc5dd1b8b3c3d2e0fa9c7d6f1
BraiderZ <luisjbc2112@gmail.com>
2023-10-07 14:37:22 -0600

    EL usuario puede agregar un valor entero

--- arreglosyrecursividad.c
+++ arreglosyrecursividad.c
@@ -2,5 +2,9 @@
 
 int main (){
 	int arreglo[] = {2, 4, 6, 23, 56, 79};
-	printf("El tercer valor del arreglo es: %d",arreglo[2]);
+	int valor;
+
+	printf("Ingrese un valor entero para verificar si se encuentra en el arreglo: ");
+    	scanf("%d", &valor);
+	printf("EL valor entero agregado es: %d",valor);
 }
