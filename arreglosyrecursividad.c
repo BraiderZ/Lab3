c4bb974472633a72ca63f6951764f8cb1ca65bb0
BraiderZ <luisjbc2112@gmail.com>
2023-10-07 15:18:06 -0600

    La funcion cumple su cometido

--- arreglosyrecursividad.c
+++ arreglosyrecursividad.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 int existeValor(int *arreglo, int valor, int longitud) {
+	
+	int existe = 0;
+
 	for (int i = 0; i < longitud; i++) {
-		printf("%d",arreglo[i]);
+		if (arreglo[i] == valor){
+			printf("El valor existe en la posicion: %d",arreglo[i]);
+			existe=1;
+			break;
+		}
 	}
-		
+
+	if (existe == 0){
+	printf("El valor no existe");
+	}
 }
 
 int main (){
 	int arreglo[] = {2, 4, 6, 23, 56, 79};
 	
-	printf("%d",arreglo[2]);
-	
 	int longitud = sizeof(arreglo)/sizeof(arreglo[0]);
-	
-	printf("longitud del arreglo: %d",longitud);
 
 	int valor;
 	
 	printf("Ingrese un valor entero para verificar si se encuentra en el arreglo: ");
     	
 	scanf("%d", &valor);
-	
-	printf("Valor: %d",valor);
 
 	existeValor(arreglo, valor, longitud);
 }
