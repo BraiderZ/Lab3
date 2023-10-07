f4e8cc7e9ed74cdd8cea5e4d424ffd263c4d62bb
BraiderZ <luisjbc2112@gmail.com>
2023-10-07 16:19:30 -0600

    Ahora existe la funcion de busqueda Binaria

--- arreglosyrecursividad.c
+++ arreglosyrecursividad.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
 
+int busquedaBinaria(int *arreglo, int valor, int longitud) {
 
-//Busqueda lineal
+        int existe=0; //Se encarga de ver si el valor existe
+
+        //Variables para calcular el valor de en medio
+        int izquierda = 0;
+        int derecha = longitud -1;
+
+        while(izquierda <= derecha){
+
+                int numeroMitad =  izquierda + (derecha - izquierda) / 2;
+
+                if (valor == arreglo[numeroMitad]){
+                        printf("Binaria-El valor existe en la posicion: %d\n",numeroMitad);
+                        existe=1;
+			
+			break;
+                }else if (valor > arreglo[numeroMitad]){
+
+                        izquierda = numeroMitad + 1;
+                }else {
+
+                        derecha = numeroMitad -1;
+                }
+        }
+	if (existe == 0){
+		printf("Binaria-El valor no existe\n");
+	}
+}
+
+
 int busquedaLineal(int *arreglo, int valor, int longitud) {
 	
 	int existe = 0; //Variable para reconocer si existe el valor en el arreglo
 
 	for (int i = 0; i < longitud; i++) {
 		if (arreglo[i] == valor){
-			printf("El valor existe en la posicion: %d",i);
+			printf("Lineal-El valor existe en la posicion: %d\n",i);
 			existe=1;
 			break;
 		}
 	}
 
 	if (existe == 0){
-	printf("El valor no existe");
+	printf("Lineal-El valor no existe\n");
 	}
 }
 
@@ -31,5 +60,5 @@ int main (){
 	scanf("%d", &valor);
 
 	busquedaLineal(arreglo, valor, longitud);
-	//busquedaBinaria(arreglo, valor, longitud);
+	busquedaBinaria(arreglo, valor, longitud);
 }
