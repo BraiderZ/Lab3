8d15fdb087dd0c5c782b861300eaca89062fe137
BraiderZ <luisjbc2112@gmail.com>
2023-10-09 18:33:40 -0600

    La funcion para encontrar el valor minimo ya funciona

--- arreglosypunteros.c
+++ arreglosypunteros.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
+int Min(int *arreglo, int longitud){
+	
+	int min = arreglo[0];
+
+	for (int i=1 ; i < longitud ; i++) {
+
+		if (arreglo[i] < min){
+			
+			min = arreglo[i];
+		}
+	}
+	return min;
+}
+
+
+
+
+
+
 int main (){
         int arreglo[] = {21, 24, 65, 3, 56, 12, 35, 15};
 
-	printf("%d\n", arreglo[6]);
-
         int longitud = sizeof(arreglo)/sizeof(arreglo[0]);
 	
-	printf("%d\n", longitud);
+	int min = Min(arreglo, longitud);
+       	
+	printf("El valor minimo del arreglo es: %d\n", min);
+	
+	return 0;	
 }
 
